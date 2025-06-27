31cde00e13b83ba4d5b0fa9ea6005660ffa97d20
Chinyelugo Jeremiah <36512525+Jeremyugo@users.noreply.github.com>
2025-06-27 14:24:55 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Dead Man Switch
+
+Personal project - scripts that execute one or more deadman logic after a specified time interval
+
+**In Progress...**
---
+++ src/strategies.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+class Strategy(ABC):
+    @abstractmethod
+    def execute(self):
+        raise NotImplementedError
+    
+
+class SendEmail(Strategy):
+    def __init__(self, ):
+        pass
+    
+    def execute(self):
+        pass
+    
+    
+    
+class DecryptData(Strategy):
+    def __init__(self, ):
+        pass
+    
+    
+    def execute(self):
+        pass
+    
+    
+    
+class DeleteData()
